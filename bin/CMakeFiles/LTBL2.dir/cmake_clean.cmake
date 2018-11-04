5355213b6137b54a838b0035170637de7ece278f
Grundkurs <commanderkeen83@googlemail.com>
2018-11-04 13:38:01 +0100

    add gitignore

---
+++ bin/CMakeFiles/LTBL2.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LTBL2.dir/source/LightDirectionEmission.cpp.obj"
+  "CMakeFiles/LTBL2.dir/source/LightPointEmission.cpp.obj"
+  "CMakeFiles/LTBL2.dir/source/LightShape.cpp.obj"
+  "CMakeFiles/LTBL2.dir/source/LightSystem.cpp.obj"
+  "CMakeFiles/LTBL2.dir/source/Sprite.cpp.obj"
+  "libLTBL2.pdb"
+  "libLTBL2.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LTBL2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
